17755e028ffd528d25b87b79c812e371f1c48a04
FloH <florian_hoehle@teilnehmer.cdemy.de>
2024-11-10 00:04:44 +0100

    Add custom led / buzzer actions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import * as nfcpcsc from 'nfc-pcsc';
-const { NFC, Reader, CONNECT_MODE_DIRECT, ACR122 }: any = nfcpcsc;
+const { NFC, Reader }: any = nfcpcsc;
 import dotenv from "dotenv";
 dotenv.config();
 
 import Database from './database';
 import { ParticipantType } from './types/database';
+
+
+const ledSuccess = [0b00101110, [0x05, 0x01, 0x01, 0x01]];
+const ledWarning = [0b11111111, [0x03, 0x01, 0x03, 0x01]];
+const ledError = [0b01001101, [0x01, 0x01, 0x05, 0x01]];
+// a) led settings: "0b" + 8 bool settings
+// 1) green will blink
+// 2) red will blink
+// 3) green state at start
+// 4) red state at start
+// 5) green state will be updated
+// 6) red state will be updated
+// 7) green on / off at the end (short amount)
+// 8) red on / off at the end (short amount)
+
+// b) blinking behaviour 4 settings each with "0x" + hex
+// 1) odd phased time x * 100ms
+// 2) even phased time x * 100ms
+// 3) number of repetitions
+// 4) buzzer setting:
+// 00 no sound
+// 01 sound if led on
+// 02 sound if led off
+// 03 always sound
+
 
 
 (async function main () {
@@ -36,53 +61,59 @@ import { ParticipantType } from './types/database';
 
         const participant: ParticipantType = await db.getUserByRfid(card.uid);
 
-        if(participant) {
-          const now = new Date();
-          const currentDateForDb = now.toLocaleDateString('en-CA').replaceAll('-', '');
-          const currentTimeForDb = now.getHours() * 60 + now.getMinutes();
-          const currentDateForHumans = now.toLocaleDateString('de-DE');
-          const currentTimeForHumans = `${now.getHours()}:${now.getMinutes()}`;
-          const blockingPeriodInMinutes = 5;
-          const todaysPresence = await db.fetchPresenceByUidAndDate(participant.tn_id, currentDateForDb);
-
-          if (todaysPresence) {
-            if (todaysPresence.anw_bis_uhrzeit) {
-              console.error(`${participant.tn_vorname} ${participant.tn_nachname} already logged for today.`);
-              console.info('Ready to READ.');
-              return;
-            }
-            if (todaysPresence.anw_von_uhrzeit) {
-              if (todaysPresence.anw_von_uhrzeit + blockingPeriodInMinutes > currentTimeForDb) {
-                console.warn(`${
-                  participant.tn_vorname} ${participant.tn_nachname
-                } was logged within the last ${blockingPeriodInMinutes} minutes`);
-                console.info('Ready to READ.');
-                return;
-              }
-
-              await db.updatePresenceWithDeparture(
-                participant.tn_id,
-                currentDateForDb,
-                currentTimeForDb,
-              )
-              console.warn(`${participant.tn_vorname} ${participant.tn_nachname}'s departure logged on ${currentDateForHumans} at ${currentTimeForHumans}`);
-              console.warn(`Time difference today: ${await db.getTimeDifferenceInMin(participant.tn_id, currentDateForDb)}min`)
-              console.info('Ready to READ.');
-              return;
-            }
-          }
-
-          await db.insertPresence(
-            participant.tn_id,
-            currentDateForDb,
-            currentTimeForDb,
-            participant.tn_modifikator,
-          )
-          console.warn(`${participant.tn_vorname} ${participant.tn_nachname}'s arrival logged on ${currentDateForHumans} at ${currentTimeForHumans}`);
-          console.info('Ready to READ.');
-          return;
+        if(!participant) {
+          await reader.led(...ledError);
+          return console.error('This Tag is not registered.');
         }
-        console.warn('NO Participant attached to this Tag.')
+
+        const now = new Date();
+        const currentDateForDb = now.toLocaleDateString('en-CA').replaceAll('-', '');
+        const currentTimeForDb = now.getHours() * 60 + now.getMinutes();
+        const currentDateForHumans = now.toLocaleDateString('de-DE');
+        const currentTimeForHumans = `${now.getHours()}:${now.getMinutes()}`;
+        const blockingPeriodInMinutes = 5;
+        const todaysPresence = await db.fetchPresenceByUidAndDate(participant.tn_id, currentDateForDb);
+
+        if (todaysPresence) {
+          if (todaysPresence.anw_bis_uhrzeit) {
+            await reader.led(...ledWarning);
+            console.error(`${participant.tn_vorname} ${participant.tn_nachname} already logged for today.`);
+            console.info('Ready to READ.');
+            return;
+          }
+          if (todaysPresence.anw_von_uhrzeit) {
+            if (todaysPresence.anw_von_uhrzeit + blockingPeriodInMinutes > currentTimeForDb) {
+              await reader.led(...ledWarning);
+              console.warn(`${
+                participant.tn_vorname} ${participant.tn_nachname
+              } was logged within the last ${blockingPeriodInMinutes} minutes`);
+              console.info('Ready to READ.');
+              return;
+            }
+
+            await db.updatePresenceWithDeparture(
+              participant.tn_id,
+              currentDateForDb,
+              currentTimeForDb,
+            )
+            await reader.led(...ledSuccess);
+            console.warn(`${participant.tn_vorname} ${participant.tn_nachname}'s departure logged on ${currentDateForHumans} at ${currentTimeForHumans}`);
+            console.warn(`Time difference today: ${await db.getTimeDifferenceInMin(participant.tn_id, currentDateForDb)}min`)
+            console.info('Ready to READ.');
+            return;
+          }
+        }
+
+        await db.insertPresence(
+          participant.tn_id,
+          currentDateForDb,
+          currentTimeForDb,
+          participant.tn_modifikator,
+        )
+        await reader.led(...ledSuccess);
+        console.warn(`${participant.tn_vorname} ${participant.tn_nachname}'s arrival logged on ${currentDateForHumans} at ${currentTimeForHumans}`);
+        console.info('Ready to READ.');
+        return;
       });
 
       reader.on('error', (error :Error) => {
